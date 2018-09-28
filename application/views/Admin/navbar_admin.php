4c4566e34dd6d373edcaeb34789db915c3e81ea7
yzuniga <y.zunigam@hotmail.com>
2018-09-28 11:45:51 -0500

    modificaciones

--- application/views/Admin/navbar_admin.php
+++ application/views/Admin/navbar_admin.php
@@ -12,32 +12,14 @@
         </div>
         <!-- Sidebar Navigation Menus-->
         <div class="main-menu">
-          <h5 class="sidenav-heading">Main</h5>
+          <h5 class="sidenav-heading">Menú</h5>
           <ul id="side-main-menu" class="side-menu list-unstyled">                  
-            <li><a href="index.html"> <i class="icon-home"></i>Home                             </a></li>
-            <li><a href="forms.html"> <i class="icon-form"></i>Forms                             </a></li>
-            <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts                             </a></li>
-            <li><a href="tables.html"> <i class="icon-grid"></i>Tables                             </a></li>
-            <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Example dropdown </a>
-              <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                <li><a href="#">Page</a></li>
-                <li><a href="#">Page</a></li>
-                <li><a href="#">Page</a></li>
-              </ul>
-            </li>
-            <li><a href="login.html"> <i class="icon-interface-windows"></i>Login page                             </a></li>
-            <li> <a href="#"> <i class="icon-mail"></i>Demo
-                <div class="badge badge-warning">6 New</div></a></li>
-          </ul>
-        </div>
-        <div class="admin-menu">
-          <h5 class="sidenav-heading">Second menu</h5>
-          <ul id="side-admin-menu" class="side-menu list-unstyled"> 
-            <li> <a href="#"> <i class="icon-screen"> </i>Demo</a></li>
-            <li> <a href="#"> <i class="icon-flask"> </i>Demo
-                <div class="badge badge-info">Special</div></a></li>
-            <li> <a href=""> <i class="icon-flask"> </i>Demo</a></li>
-            <li> <a href=""> <i class="icon-picture"> </i>Demo</a></li>
+            <li><a href="<?php echo site_url('index.php/plataforma/login_admin') ?>"> <i class="icon-home"></i>Inicio                                </a></li>
+            <li><a href="Admin/empleados.php"> <i class="fa fa-male"></i>Empleados                            </a></li>
+            <li><a href="Admin/clientes.php"> <i class="fa fa-briefcase"></i>Clientes                       </a></li>
+            <li><a href="tables.html"> <i class="fa fa-archive"></i>Campañas                          </a></li>
+            <li><a href="login.html"> <i class="fa fa-child"></i>Mi perfil
+                </a></li>            
           </ul>
         </div>
       </div>
