7decaecd4e1ed905d0b7268df3d3deae57f8e708
yzuniga <y.zunigam@hotmail.com>
2018-12-04 03:19:47 -0600

    red semantica parte 1.2.1 04-12-2018

--- application/views/CommunityManager/red.php
+++ application/views/CommunityManager/red.php
@@ -1,12 +1,5 @@
 <script src="<?php echo base_url(); ?>js/vis.min.js"></script>
-<style>
-.vertical-line{   
-    border:         none;
-    border-left:    1px solid hsla(200, 10%, 50%,100);
-    height:         10vh;
-    width:          1px;       
-}
-</style>
+
 <?php
 
 $db = mysqli_connect("localhost", "root","","freelancer");
@@ -89,8 +82,8 @@ while($row2 = mysqli_fetch_assoc($aristas)){
                     
                     <!-- Inicia desplazador -->
                     <p class="float-right">
-                    <a class="btn btn-warning" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Editar</a>
-                    <button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Eliminar</button>
+                    <!--a class="btn btn-warning" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Editar <i class="fa fa-chevron-down" aria-hidden="true"></i></a-->
+                    <!--button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Eliminar <i class="fa fa-chevron-down" aria-hidden="true"></i></button-->
                     </p>
                     <div class="row">
                     <div class="col">
@@ -128,25 +121,26 @@ while($row2 = mysqli_fetch_assoc($aristas)){
                     <div class="col">
                         <div class="collapse multi-collapse" id="multiCollapseExample2">
                         <div class="card card-body">
-                            <form action="eliminar_nodo" method="post">                               
+                            <form id="delete-form" method="post">                               
                                 <div class="form-group col">
                                     <label  class=" form-control-label">Nodo:</label>
-                                    <select name="" id="" class="form-control cc-name valid">
+                                    <select name="padre_delete" id="padre_delete" class="form-control cc-name valid">
                                         <?php 
                                             foreach ($nodos_red as $key => $value) {           
                                         ?>
                                         <option value = "<?php echo($value->id_nodo)?>" >
                                             <?php echo($value->nombre) ?>
                                         </option>
-                                        <?php
-                                            }               
-                                        ?>
+                                        
                                     </select>
                                 </div>  
                                 <div class="form-group col">     
                                     <br>                                  
                                     <!--input  class="btn btn-primary "  name="guardar"  type="submit" value="Guardar" id="guardar"/-->
-                                    <button class="btn-danger btn" style=" margin-top: 15px;" type="submit" id="eliminar" name="eliminar" value="">Eliminar</button>
+                                    <button class="btn-danger btn" style=" margin-top: 15px;" type="submit" id="<?php echo $value->id_nodo ?>" name="eliminar" value="">Eliminar <?php echo $value->id_nodo?></button>
+                                    <?php
+                                            }               
+                                        ?>
                                 </div> 
                                 <hr>
                             </form>
@@ -284,4 +278,32 @@ while($row2 = mysqli_fetch_assoc($aristas)){
         };
         var network = new vis.Network(container, data, options);        
         
+$(document).ready(function() {
+          $('#insert_form').on("submit", function(event){  
+          event.preventDefault();  
+                if(estado==0){
+                        $.ajax({  
+                            url:"insert.php",  
+                            method:"POST",  
+                            data:$('#insert_form').serialize(),  
+                            beforeSend:function(){  
+                                $('#insert').val("Guardando");  
+                            },  
+                            success:function(data){  
+                                
+                                $('#insert_form')[0].reset();  
+                                $('#add_data_Modal').modal('hide');
+                                $("#campaña_table").html(data);  
+                                $('#bootstrap-data-table').DataTable({ 
+                                    "destroy": true, //use for reinitialize datatable
+                                });                        
+                            },error: function(xhr, textStatus, errorThrown){
+                            alert('Ocurrió un error, intente más tarde');
+                        } 
+                    }); 
+                }
+            
+      
+      });
+    });
 </script>
