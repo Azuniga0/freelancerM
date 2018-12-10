789d630724e6905ff803a2a3a2c70ef5e869137e
yzuniga <y.zunigam@hotmail.com>
2018-12-09 23:43:38 -0600

    mejoras en cm 1.2 asignacion de usuarios a nodos 09-12-2018

--- red/delete.php
+++ red/delete.php
@@ -1,9 +1,13 @@
 <?php
-include('db.php');
-if($_POST['id'])
-{
-$id=mysql_real_escape_string($_POST['padre_delete']);
-$delete = "DELETE FROM `nodos` WHERE id_nodo='$id'";
-mysql_query( $delete);
-}
+    $connect = mysqli_connect("localhost", "root","","freelancer");
+    $connect->set_charset("utf8"); 
+
+
+    if($_POST["action"] == "delete"){
+        $query = "DELETE FROM nodos WHERE id_nodo= '".$_POST["id_nodo_delete"]."'";    
+        if(mysqli_query($connect, $query)){
+            //echo 'Node Deleted from Database';
+            echo 'refresh';
+        }
+    }
 ?>
