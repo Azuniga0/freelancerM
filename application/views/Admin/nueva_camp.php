6e563ed77a7b1db66929ae2d1b9eda32950e76a7
yzuniga <y.zunigam@hotmail.com>
2018-12-10 09:59:37 -0600

    modificacion admin 1.0 se modifico el orden del formulario de la creacion de camp 10-12-2018

--- application/views/Admin/nueva_camp.php
+++ application/views/Admin/nueva_camp.php
@@ -11,48 +11,9 @@
                         </div> 
                     </div>
                     <div class="row">   
-                        <form class="col-md-12" method="POST" action="nueva_camp">
+                        <form class="col-md-12" method="POST" action="nueva_camp">  
                             <div class="form-row">
-                                <div class="col-md-12 mb-3">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="validationTooltipUsernamePrepend">Nombre de campaña:</span>
-                                        </div>
-                                        <input type="text" class="form-control" id="validationTooltipUsername" placeholder="" name=" nombre" aria-describedby="validationTooltipUsernamePrepend" required>
-                                        <div class="invalid-tooltip">
-                                            Por favor, inserte un nombre válido
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="form-group col-6">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="cm_id">Community Manager:</span>
-                                        </div>        
-
-                                        <?php
-                                            $database=mysqli_connect("localhost", "root","","freelancer");
-
-                                            $query = "SELECT * FROM usuarios join empleados on empleados.id_usuario_empleado = usuarios.id_usuario WHERE rol = 2 and id_estado_us = 1 ORDER BY id_usuario ASC";
-                                            $result = mysqli_query($database,$query) or die("no se encontraron datos");
-                                            mysqli_set_charset($database,"utf8");
-                                            ?>
-
-                                            <select class="form-control" id="rol" aria-describedby="cm_id" name="id_community" required>                                            
-                                                <?php     
-                                                    while ($row = mysqli_fetch_array($result)){
-                                                        echo "<option value='" . $row['id_usuario'] . "'>" . $row['nombre_empleado'].' '.$row['apaterno_empleado'].' '.$row['amaterno_empleado']. "</option>";
-                                                    }
-                                                ?>        
-                                            </select>
-                                        <div class="invalid-tooltip">
-                                            Por favor, seleccione un rol válido
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group col-6">
+                            <div class="form-group col-6">
                                     <div class="input-group">
                                         <div class="input-group-prepend">
                                             <span class="input-group-text" id="cm_id">Empresa:</span>
@@ -78,8 +39,71 @@
                                         </div>
                                     </div>  
                                 </div>
+                                <div class="form-group col-6">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="cm_id">Community Manager:</span>
+                                        </div>        
+
+                                        <?php
+                                            $database=mysqli_connect("localhost", "root","","freelancer");
+
+                                            $query = "SELECT * FROM usuarios join empleados on empleados.id_usuario_empleado = usuarios.id_usuario WHERE rol = 2 and id_estado_us = 1 ORDER BY id_usuario ASC";
+                                            $result = mysqli_query($database,$query) or die("no se encontraron datos");
+                                            mysqli_set_charset($database,"utf8");
+                                            ?>
+
+                                            <select class="form-control" id="rol" aria-describedby="cm_id" name="id_community" required>                                            
+                                                <?php     
+                                                    while ($row = mysqli_fetch_array($result)){
+                                                        echo "<option value='" . $row['id_usuario'] . "'>" . $row['nombre_empleado'].' '.$row['apaterno_empleado'].' '.$row['amaterno_empleado']. "</option>";
+                                                    }
+                                                ?>        
+                                            </select>
+                                        <div class="invalid-tooltip">
+                                            Por favor, seleccione un rol válido
+                                        </div>
+                                    </div>
+                                </div>                                
+                            </div>
+                            <div class="form-row">
+                                <div class="col-md-12 mb-3">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="validationTooltipUsernamePrepend">Nombre de campaña:</span>
+                                        </div>
+                                        <input type="text" class="form-control" id="validationTooltipUsername" placeholder="" name=" nombre" aria-describedby="validationTooltipUsernamePrepend" required>
+                                        <div class="invalid-tooltip">
+                                            Por favor, inserte un nombre válido
+                                        </div>
+                                    </div>
+                                </div>
                             </div>                            
                             <div class="form-row">
+                            <div class="form-group col-md-12">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="validationTooltipUsernamePrepend">Objetivo:</span>
+                                        </div>
+                                        <textarea  class="form-control" rows="2" id="validationTooltipUsername" placeholder="Objetivo de la campaña..." name="objetivo" aria-describedby="validationTooltipUsernamePrepend"  required></textarea>
+                                    </div>
+                                </div>
+                                <br><br>
+                            </div>                  
+                            <div class="form-row"> 
+                            <div class="form-group col-6">
+                                    <div class="input-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="validationTooltipUsernamePrepend">Fecha de término:</span>
+                                            </div>
+                                            <input type="date" class="form-control" id="validationTooltipUsername" placeholder="" name="fecha_termino" aria-describedby="validationTooltipUsernamePrepend" required min="<?php echo  date('Y-m-d'); ?>">
+                                            <div class="invalid-tooltip">
+                                                Por favor, inserte un usuario válido
+                                            </div>
+                                        </div>   
+                                    </div>
+                                </div>
                                 <div class="form-group col-6">
                                     <div class="input-group">
                                         <div class="input-group-prepend">
@@ -90,37 +114,15 @@
                                             Por favor, inserte un usuario válido
                                         </div>
                                     </div>                                   
-                                </div>
-                                <div class="form-group col-6">
-                                    <div class="input-group">
-                                        <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="validationTooltipUsernamePrepend">Fecha de término:</span>
-                                        </div>
-                                        <input type="date" class="form-control" id="validationTooltipUsername" placeholder="" name="fecha_termino" aria-describedby="validationTooltipUsernamePrepend" required min="<?php echo  date('Y-m-d'); ?>">
-                                        <div class="invalid-tooltip">
-                                            Por favor, inserte un usuario válido
-                                        </div>
-                                    </div>   
-                                    </div>
-                                </div>
-                                <br><br>
-                            </div>                  
-                            <div class="form-row">                            
-                                <div class="form-group col-md-6">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="validationTooltipUsernamePrepend">Objetivo:</span>
-                                        </div>
-                                        <textarea  class="form-control" rows="3" id="validationTooltipUsername" placeholder="Objetivo de la campaña..." name="objetivo" aria-describedby="validationTooltipUsernamePrepend"  required></textarea>
-                                    </div>
-                                </div>
+                                </div>                         
+                            </div>
+                            <div class="form-row">
                                 <div class="input-group mb-3 col-6">
                                     <div class="custom-file">
                                         <input type="file" class="custom-file-input" id="inputGroupFile01" name="picture" >
                                         <label class="custom-file-label" for="inputGroupFile01">Presiona aquí</label>
                                     </div>
-                                </div>                                
+                                </div>
                             </div>
                             <input class="btn-primary btn" style=" margin-top: 15px; float:right;" type="submit" value="Guardar" name="register" >
                         </form>
